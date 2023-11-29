aab8f6edd9ee13e19aea2ab32c55b34f71683156
althierfson <althierfson.lima.109@ufrn.edu.br>
2023-11-29 18:09:02 -0300

    up comment

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,30 @@ import 'package:target_sistemas_test/container_injection.dart';
 import 'package:target_sistemas_test/route_generato.dart';
 
 /*
-Para fazer teste no APP use uma das seguintes credencia:
-  username: kminchelle,
-  password: 0lelplR
+  O aplicativo utiliza a API DummyJSON, uma API fictícia que possibilita
+  simular diversas funcionalidades, incluindo a autenticação.
+
+  O processo de autenticação é realizado por meio de uma requisição
+  POST simples no endpoint https://dummyjson.com/auth/login.
+
+  Essa requisição deve ter um JSON no body no seguinte formato:
+
+      {
+          username: 'username',
+          password: 'password'
+      }
+
+  Para fazer teste no APP use uma das seguintes credencias:
+
+      username: kminchelle,
+      password: 0lelplR
 
   Ou
 
-  username: atuny0,
-  password: 9uQFF1Lh
+      username: atuny0,
+      password: 9uQFF1Lh
+
+  Você pode encontrar outras credencias nesse link: https://dummyjson.com/users.
 */
 
 void main() async {
